6662fd70c1003d429e9ec0833b13268bfdde0e1f
Patrick Robotham <patrick@bellroy.com>
2017-03-17 16:55:00 +1100

    Fixed some bugs

--- R/data.R
+++ R/data.R
@@ -4,12 +4,17 @@
 #'
 #'  @format A dataframe with 7 rows and 4 variables
 #'  \describe{
-#'    \item{package} The name of the package
-#'    \item{Dec2016} The number of downloads from CRAN in December, 2016
-#'    \item{Jan2017} The number of downloads from CRAN in January, 2017
-#'    \item{change_percentage} The relative difference in downloads between the January 2017 and December 2018
-#'
-#'  }
+#'    \itemize{
+#'    \item{package}
+#'    The name of the package
+#'    \item{Dec2016}
+#'    The number of downloads from CRAN in December, 2016
+#'    \item{Jan2017}
+#'    The number of downloads from CRAN in January, 2017
+#'    \item{change_percentage}
+#'    The relative difference in downloads between
+#'    the January 2017 and December 2018
+#'  }}
 #'  Data sourced from the dlstats package
 #'  @source \url{https://cran.r-project.org/package=dlstats}
 #'
--- R/geom_icon.R
+++ R/geom_icon.R
@@ -1,7 +1,4 @@
-library(ggplot2)
-library(grid)
-
-GeomIcon <- ggproto("GeomIcon", Geom,
+GeomIcon <- ggplot2::ggproto("GeomIcon", ggplot2::Geom,
   required_aes = c("x", "y", "file_path"),
   default_aes = ggplot2::aes(width = 0.1, height = 0.1),
 
@@ -16,14 +13,14 @@ GeomIcon <- ggproto("GeomIcon", Geom,
 )
 
 iconGrob <- function(x,y,file_path, width = 0.1, height = 0.1){
-  grob(x = x, y = y, file_path = file_path, cl = "icon",
+  grid::grob(x = x, y = y, file_path = file_path, cl = "icon",
        width = width, height = height)
 }
 
 drawDetails.icon <- function(x, recording = FALSE){
-  for(i in seq_along(x$file_path)){
+  for (i in seq_along(x$file_path)) {
     image <- png::readPNG(x$file_path[[i]])
-    grid.raster(x$x[i], x$y[i], image = image,
+    grid::grid.raster(x$x[i], x$y[i], image = image,
                 width = x$width[i], height = x$height[i])
   }
 }
@@ -40,7 +37,10 @@ drawDetails.icon <- function(x, recording = FALSE){
 #' @details
 #'
 #' @examples
+#' library(dplyr)
+#' library(ggplot2)
 #' data(tidyverse_downloads)
+#'
 #' downloads_with_icon <- tidyverse_downloads %>%
 #'    mutate(file_path = system.file("icons",paste0(package,".png"),"ggicons"))
 #'  ggplot(downloads_with_icon, aes(x = Jan2017, y = change_percentage, file_path = file_path)) +
@@ -49,7 +49,7 @@ drawDetails.icon <- function(x, recording = FALSE){
 geom_icon <- function(mapping = NULL, data = NULL, stat = "identity",
                       position = "identity", na.rm = FALSE, show.legend = NA,
                       inherit.aes = TRUE, ...){
-  layer(
+  ggplot2::layer(
     geom = GeomIcon,  mapping = mapping, data = data, stat = stat,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, ...)
